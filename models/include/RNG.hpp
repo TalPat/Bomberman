954b92dcb0a053b40221e5e45f077d74447e31d4
Charl Moller <cmoller@student.wethinkcode.co.za>
2019-09-19 19:51:55 +0200

    test

--- models/include/RNG.hpp
+++ models/include/RNG.hpp
@@ -8,7 +8,6 @@ class RNG{
 		std::random_device rd;
 		int randNum;
 		std::uniform_int_distribution<int> dist(min, max);
-		// returns number between min and max (including max)
 		randNum = dist(rd);
 		return randNum;
     }
