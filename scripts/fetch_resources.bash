bf6768f0e6755fb68d6611d4224df20e9d10d73d
Werner SEEGERS <wseegers@c1r2s1.wethinkcode.co.za>
2019-10-21 13:10:46 +0200

    fetch_working

--- scripts/fetch_resources.bash
+++ scripts/fetch_resources.bash
@@ -1,12 +1,14 @@
 echo -n "Fetching resources, this may take a minuite..."
 
-TEMP_PATH="~/tmp_bomberman_res"
+TEMP_PATH="$HOME/tmp_bomberman_res"
 SOUND_PATH="sound/res"
 GRAPHIX_PATH="renderer/res"
 
 git clone https://github.com/wseegers/bomberman $TEMP_PATH &> /dev/null
 
-cp -r $TEMP_PATH/$SOUND_PATH ./$SOUND_PATH
-cp -r $TEMP_PATH/$GRAPHIX_PATH ./$GRAPHIX_PATH
+cp -r "$TEMP_PATH"/"$SOUND_PATH""_old" ./$SOUND_PATH
+cp -r "$TEMP_PATH"/"$GRAPHIX_PATH""_old" ./$GRAPHIX_PATH
+
+rm -rf $TEMP_PATH
 
 echo "done"
