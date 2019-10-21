64571de6b7ce3d5932ab254ba4d56a9e281345fe
GJSteyn <gsteyn@student.wethinkcode.co.za>
2019-10-21 11:24:00 +0200

    Pickups to and from string

--- models/src/Pickups.cpp
+++ models/src/Pickups.cpp
@@ -81,3 +81,33 @@ void Pickups::update(GameState &state)
 		}
 	}
 }
+
+std::string Pickups::to_string()
+{
+	std::ostringstream sstream;
+
+	sstream << this->_pickups.size() << "\n";
+
+	for (sPickup pickup : this->_pickups)
+	{
+		sstream << pickup.position.x << " " << pickup.position.y << " " << pickup.type << " ";
+	}
+	sstream << '\n';
+
+	return sstream.str();
+}
+
+void Pickups::from_string(std::istringstream &istream)
+{
+	int n;
+	istream >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		sPickup pickup;
+		int type;
+		istream >> pickup.position.x >> pickup.position.y >> type;
+		pickup.type = PickupType(type);
+		this->_pickups.push_back(pickup);
+	}
+}
